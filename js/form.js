e14c7c92d7f11909292ad217310a5e23ff07eeb0
Dmitry Zakharov <dzaharov794@gmail.com>
2021-06-21 21:59:48 +0300

    Добавляет функцию по очищению полей после закрытия формы

--- js/form.js
+++ js/form.js
@@ -1,27 +1,45 @@
-const uploadButton = document.querySelector('#upload-file');
-const editorForm = document.querySelector('.img-upload__overlay');
-const page = document.querySelector('body');
-const cancelButton = document.querySelector('#upload-cancel');
+import {isEscEvent} from './utils.js';
 
-const closeUpload = (evt) => {
-  document.removeEventListener('keydown', closeUpload);
-  cancelButton.removeEventListener('click', closeUpload);
-  editorForm.classList.add('hidden');
-  page.classList.remove('modal-open');
+const uploadInput = document.querySelector('#upload-file');
+const cancelButton = document.querySelector('#upload-cancel');
+const editorForm = document.querySelector('.img-upload__overlay');
+const hashtagInput =  document.querySelector('.text__hashtags');
+const commentText = document.querySelector('.text__description');
+const page = document.querySelector('body');
+
+const resetInputs = () => {
+  uploadInput.value = '';
+  hashtagInput.value ='';
+  commentText.value = '';
+};
+
+const closeUploadEsc = (evt) => {
   if (isEscEvent(evt)) {
     evt.preventDefault();
-    cancelButton.classList.add('hidden');
+    editorForm.classList.add('hidden');
     page.classList.remove('modal-open');
+    resetInputs();
+    document.removeEventListener('keydown', closeUploadEsc);
+    //eslint-disable-next-line no-use-before-define
+    cancelButton.removeEventListener('click', closeUpload);
   }
-}
+};
+
+const closeUpload = () => {
+  resetInputs();
+  editorForm.classList.add('hidden');
+  page.classList.remove('modal-open');
+  document.removeEventListener('keydown', closeUploadEsc);
+  cancelButton.removeEventListener('click', closeUpload);
+};
 
 const openUpload = () => {
   editorForm.classList.remove('hidden');
   page.classList.add('modal-open');
-  document.addEventListener('keydown', closeUpload);
+  document.addEventListener('keydown', closeUploadEsc);
   cancelButton.addEventListener('click', closeUpload);
-}
+};
 
-uploadButton.addEventListener('click', () => {
+uploadInput.addEventListener('click', () => {
   openUpload();
-})
+});
