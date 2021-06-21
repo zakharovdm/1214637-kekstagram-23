47595d5621991629301ad3b736518cfeae310224
Dmitry Zakharov <dzaharov794@gmail.com>
2021-06-21 21:36:12 +0300

    Исправляет баг обработчика нажатия esc для закрытия окна

--- js/controls.js
+++ js/controls.js
@@ -4,22 +4,28 @@ const page = document.querySelector('body');
 const cancelButton = document.querySelector('.big-picture__cancel');
 const popup = document.querySelector('.big-picture');
 
-const closeModal = (evt) => {
-  document.removeEventListener('keydown', closeModal);
-  cancelButton.removeEventListener('click', closeModal);
-  popup.classList.add('hidden');
-  page.classList.remove('modal-open');
+const closeModalEsc = (evt) => {
   if (isEscEvent(evt)) {
     evt.preventDefault();
     popup.classList.add('hidden');
     page.classList.remove('modal-open');
+    document.removeEventListener('keydown', closeModalEsc);
+    //eslint-disable-next-line no-use-before-define
+    cancelButton.removeEventListener('click', closeModal);
   }
+};
+
+const closeModal = () => {
+  document.removeEventListener('keydown', closeModalEsc);
+  cancelButton.removeEventListener('click', closeModal);
+  popup.classList.add('hidden');
+  page.classList.remove('modal-open');
 };
 
 const openModal = () => {
   popup.classList.remove('hidden');
   page.classList.add('modal-open');
-  document.addEventListener('keydown', closeModal);
+  document.addEventListener('keydown', closeModalEsc);
   cancelButton.addEventListener('click', closeModal);
 };
 
