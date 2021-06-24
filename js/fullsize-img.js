8d56f498259b6c76e3d9f3a198198b48c049c11c
Dmitry Zakharov <dzaharov794@gmail.com>
2021-06-24 23:12:02 +0300

    Добавляет функцию по очистке комментариев

--- js/fullsize-img.js
+++ js/fullsize-img.js
@@ -6,7 +6,12 @@ const commentTemplate = commentsList.querySelector('.social__comment');
 const commentsCount = popup.querySelector('.social__comment-count');
 const commentsLoader = popup.querySelector('.comments-loader');
 
+const clearComments = () => {
+  commentsList.querySelectorAll('.social__comment').forEach((element) => element.remove());
+}
+
 const renderComments = (comments) => {
+  clearComments();
   const commentsListFragment = document.createDocumentFragment();
   comments.forEach(({avatar, name, message}) => {
     const commentElement = commentTemplate.cloneNode(true);
@@ -15,13 +20,11 @@ const renderComments = (comments) => {
     commentElement.querySelector('.social__text').textContent = message;
     commentsListFragment.appendChild(commentElement);
   });
-  commentsList.replaceWith(commentsListFragment);
+  commentsList.appendChild(commentsListFragment);
 };
 
 const renderFullsize = ({url, likes, comments, description}) => {
   openModal();
-  commentsCount.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
   popup.querySelector('.big-picture__img').querySelector('img').src = url;
   popup.querySelector('.likes-count').textContent = likes;
   popup.querySelector('.comments-count').textContent = comments.length;
