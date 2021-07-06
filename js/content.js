d9b4320f428211baa60e06b3920aa11f0b8557ec
Dmitry Zakharov <dzaharov794@gmail.com>
2021-07-06 14:14:02 +0300

    Добавляет функцию по показу случайных фотографий

--- js/content.js
+++ js/content.js
@@ -1,13 +1,16 @@
+import {setPosts} from './storage.js';
 import {getData} from './api.js';
 import {showAlert} from './utils.js';
 import {renderThumbnails} from './thumbnail.js';
 import {renderFullsize} from './fullsize-img.js';
+import {startFilters} from './filter.js';
 const uploadFile = document.querySelector('.img-upload');
 const picturesList = document.querySelector('.pictures');
 
 const renderContent = () => {
   getData(showAlert).then((posts) => {
     renderThumbnails(posts);
+    setPosts(posts);
     picturesList.addEventListener('click', (evt) => {
       const findFullsize = (element) => {
         if (element.id === Number(evt.target.dataset.id)) {
@@ -22,5 +25,7 @@ const renderContent = () => {
 uploadFile.addEventListener('click', (evt) => {
   evt.stopPropagation();
 });
+
+startFilters();
 
 export {renderContent};
--- js/filter.js
+++ js/filter.js
@@ -1,1 +1,41 @@
-/*Функционал по фильтрации изображений*/
+import {getRandomPositiveInteger, hasDuplicates} from './utils.js';
+import {getPosts} from './storage.js';
+import {renderThumbnails} from './thumbnail.js';
+const imgFilters = document.querySelector('.img-filters');
+const filterDefaultBtn = imgFilters.querySelector('#filter-default');
+const filterRandomBtn = imgFilters.querySelector('#filter-random');
+const filterDiscussedBtn = imgFilters.querySelector('#filter-discussed');
+const RANDOM_COUNT = 10;
+
+const clearPosts = () => {
+  const pictures = document.querySelectorAll('.picture');
+  pictures.forEach((picture) => picture.remove());
+}
+
+const startFilters = () => {
+  imgFilters.classList.remove('img-filters--inactive');
+
+  const showRandom = () => {
+    clearPosts();
+    filterDefaultBtn.classList.remove('img-filters__button--active');
+    filterRandomBtn.classList.add('img-filters__button--active');
+    let randomPosts = [];
+    const posts = getPosts();
+    const counter = Math.min(posts.length, RANDOM_COUNT);
+    const createRandomArray = () => {
+      for (let i = 0; i < counter; i++) {
+        randomPosts.push(posts[getRandomPositiveInteger(0, posts.length - 1)]);
+      }
+    }
+    createRandomArray();
+    while (hasDuplicates(randomPosts)) {
+      randomPosts = [];
+      createRandomArray();
+    };
+    renderThumbnails(randomPosts);
+  }
+
+  filterRandomBtn.addEventListener('click', showRandom);
+}
+
+export {startFilters};
